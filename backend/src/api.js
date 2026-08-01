e91488873d5fddac97d4cd72e59f7393dd010b99
manu-fiuba <mperezc@fi.uba.ar>
2026-08-01 21:53:37 +0000

    /api/status se asegura que contecta con la DB

--- backend/src/api.js
+++ backend/src/api.js
@@ -1,14 +1,27 @@
 const express = require('express');
-const app = express();
+const pool = require('./db.js')
 
+const app = express();
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
-app.get('/api/status', (req, res) => {
-  res.json({
-    status: 'Running',
-  });
+app.get('/api/status', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW() as server_time');
+    
+    res.status(200).json({
+        status: 'success',
+        message: 'API funcionando correctamente',
+        db_time: result.rows[0].server_time
+    });
+} catch (error) {
+    console.error('Fallo en la conexión a la base de datos:', error);
+    res.status(500).json({ 
+        status: 'error', 
+        message: 'Error de conexión' 
+    });
+}
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
